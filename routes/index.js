717fed81bfa21dfc75af04608aa242a4e025c5eb
malenavero <malena@codigolibre.coop.ar>
2025-05-15 20:10:27 -0300

    Agregar enlace a la vista principal para redirigir a productos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+// index.js
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Huellitas Felices',
+    productosUrl: '/productos'
+  });
 });
 
 module.exports = router;

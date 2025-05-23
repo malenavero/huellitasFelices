d9458c6e7ddfb500531981df210efff06c5808f0
malenavero <malena@codigolibre.coop.ar>
2025-05-23 18:45:04 -0300

    Borras base de datos para sumar al git ignore,  y iniciarlas la primera vez

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { setupSwagger } = require('./swagger.js');
+const ensureDataFilesExist = require('./initDataFiles');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productosRouter = require('./routes/productos.routes');
@@ -18,9 +19,13 @@ const methodOverride = require('method-override');
 const app = express();
 setupSwagger(app);
 
+// Me aseguro de que si no existe la base de datos json la cree con arrays vacios
+ensureDataFilesExist();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -62,18 +67,6 @@ app.use((err, req, res, next) => {
     error: process.env.NODE_ENV === 'development' ? err : {}
   });
 });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 
 // Explicitamos el puerto
 const port = process.env.PORT || 3000;
